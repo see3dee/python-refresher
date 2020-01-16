815b74c07bd27d40028947f22116d7f887e313fd
John Munsey <see3dee@gmail.com>
2020-01-16 08:53:08 -0800

    returns complete

---
+++ FunctionReturn.py
@@ -0,0 +1,11 @@
+def add(x,y):
+    if (x+y) >= 10:
+        print(f"Looks like x + y is greater or equal to 10: {x + y}")
+        return x + y
+    else:
+        print(f"Looks like x + y is less than 10: {x + y}")
+        return x + y
+
+
+add(6, 4)
+
