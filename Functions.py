c6819cb60896400e29d879234699a0ff5a7fc96a
John Munsey <see3dee@gmail.com>
2020-01-15 19:07:30 -0800

    functions update

---
+++ Functions.py
@@ -0,0 +1,28 @@
+# def hello():
+#     print("Hello!")
+#
+# hello()
+
+# def user_age_in_seconds():
+#     user_age = int(input("Please enter your age: "))
+#     user_age_seconds = user_age * 365 * 24 * 60 * 60
+#     print(f"Your age in seconds is {user_age_seconds}")
+#
+# user_age_in_seconds()
+
+# def add_2_numbers(x, y):  #function with parameters
+#     sum = x + y
+#     print(sum)
+#
+#
+# add_2_numbers(4, 5)
+# # Function with Arguments
+
+#
+# def hello(name, surname):
+#     print(f"Hello {name} {surname}!!!")
+#
+#
+# hello("Bob", "Smith")
+#
+# hello(surname="Smith", name="Bob")
