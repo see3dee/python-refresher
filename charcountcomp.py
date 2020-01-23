a05fc15a4c8f736c0ed190d3ca5530e396448895
John Munsey <see3dee@gmail.com>
2020-01-23 11:40:31 -0800

    sooooo close...

--- charcountcomp.py
+++ charcountcomp.py
@@ -1,14 +1,22 @@
-string_input = "aaabbbbccdff"
-len_string = (len(string_input)-1)
-print(len_string)
-for i in range(0, len_string):
-    char_cnt = 0
-    for char in string_input:
-        if char == string_input[i]:
-            # print(f" {char} {string_input[i]} {i} {char_cnt}")
+string_input = "aabbbc"
+output_string = ""
+compares = (len(string_input)-1)
+print(compares)
+char_cnt = 1
+for i in range(0, compares):
+        if string_input[i] == string_input[i+1]:
             char_cnt += 1
+            print(f" first char:{string_input[i]} 2nd char:{string_input[i+1]} i = {i} count = {char_cnt}")
+            output_string = output_string + string_input[i+1] + str(char_cnt)
         else:
-            print(f" In the else {string_input[i]} {char_cnt}")
+            char_cnt = 1
+            print(f" in else first char:{string_input[i]} 2nd char:{string_input[i+1]} i = {i} count = {char_cnt}")
+            print(output_string)
+
+
+
+
+
 
 
 
