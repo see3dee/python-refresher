5f588657654143a7b43f4e55cd4bdc659294ce1f
John Munsey <see3dee@gmail.com>
2020-09-12 10:03:11 -0700

    added comments to exercise code

--- Functions.py
+++ Functions.py
@@ -1,28 +1,39 @@
-# def hello():
-#     print("Hello!")
-#
-# hello()
+def hello():
+    print("Hello!")
 
-# def user_age_in_seconds():
-#     user_age = int(input("Please enter your age: "))
-#     user_age_seconds = user_age * 365 * 24 * 60 * 60
-#     print(f"Your age in seconds is {user_age_seconds}")
-#
-# user_age_in_seconds()
+hello()
 
-# def add_2_numbers(x, y):  #function with 2 parameters
-#     sum = x + y
-#     print(sum)
-#
-#
-# add_2_numbers(4, 5)cd s
-# # Function with Arguments
+def user_age_in_seconds():
+    user_age = int(input("Please enter your age: "))
+    user_age_seconds = user_age * 365 * 24 * 60 * 60
+    print(f"Your age in seconds is {user_age_seconds}")
 
-#
-# def hello(name, surname):
-#     print(f"Hello {name} {surname}!!!")
-#
-#
-# hello("Bob", "Smith")
-#
-# hello(surname="Smith", name="Bob")
+user_age_in_seconds()
+
+def add_2_numbers(x, y):  #function with 2 parameters
+    sum = x + y
+    print(sum)
+
+
+add_2_numbers(4, 5)
+# Function with Arguments
+
+
+def hello(name, surname):
+    print(f"Hello {name} {surname}!!!")
+
+
+hello("Bob", "Smith")
+
+hello(surname="Smith", name="Bob")
+
+
+# Order of operations
+def add_friends():  # Read 1st - python creates a callable variable
+    friends.append("Rolf") # NOT READ until called  # Read 4th
+
+
+friends = []  # Read 2nd (Better to have global variables at the top...
+# but as long as the variable exists BEFORE the function is called - it runs.
+add_friends()  # Read 3rd
+print(friends)  # Read 5th
