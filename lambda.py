ad1df99156c8565bba2ae1b2fb2b02eb9da217a5
John Munsey <see3dee@gmail.com>
2020-09-14 10:54:22 -0700

    lambda refresher

--- lambda.py
+++ lambda.py
@@ -1,22 +1,28 @@
-# add = lambda x, y: x + y
+# Lambda keyword to create anonymous functions
+lambda x, y: x + y  # 1. lambda Keyword, 2. Parameters,  3. ":" , 4. Return Value
+# you CAN name the anonymous function
+add = lambda x, y: x + y
+sum = (add(5, 7))
+print(sum)
+
+# using lambda without naming it needs to occur in one line
 #
-# sum = (add(5, 7))
-# print(sum)
-#
-# # using lambda without naming it needs to occur in one line
-#
-# def double(x):
-#     return x * 2
+def double(x):
+    return x * 2
 
 
 sequence = [1, 3, 5, 6, 9]
-# doubled = [x * 2 for x in sequence if x > 5]  # !!! list comprehension is more Pythonic
-# print(doubled)
+doubled1 = [x * 2 for x in sequence if x > 5]  # !!! list comprehension is more Pythonic
+doubled2 = [double(x) for x in sequence]  # !!! list comprehension is more Pythonic
+print(doubled1)
+print(doubled2)
 
-# you CAN use the map function to accomplish the same thing more like other languages!
+# you CAN ALSO use the map function to accomplish the same thing more like other languages!
+# And this is where the lambda expression is really useful!!
 # doubled = list.map(double, sequence)
 #  ^^using lambda^^
-doubled = [(lambda x: x*2, sequence)]
+# doubled = list(map(double, sequence))
+doubled = list(map(lambda x: x*2, sequence))
 
 print(doubled)
 
