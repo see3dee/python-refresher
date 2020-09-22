32fa77b60c7c485be1f0b101127455b72c635afa
John Munsey <see3dee@gmail.com>
2020-09-22 11:10:37 -0700

    individual args and kwargs - more

--- args_kwargs.py
+++ args_kwargs.py
@@ -12,13 +12,22 @@
 #
 #
 # print_individual_kwargs(value1=2, value2=9)
+#
+# def create_dictionary(*args):
+#     dict = {}
+#     for i, arg in enumerate(args):
+#         print(i, arg)
+#         dict[i] = arg
+#         print(dict)
+#
+#
+# create_dictionary(2, 5, 8, 'cat', {'key1': 2, 'key2': 7})
+#
+# def print_kwargs(**kwargs):
+#     for key, value in kwargs.items():
+#         print(key, value)
+#
+#
+# print_kwargs(dog='pet', cat='coco', rat={3: 'pop'})
 
-def create_dictionary(*args):
-    dict = {}
-    for i, arg in enumerate(args):
-        print(i, arg)
-        dict[i] = arg
-        print(dict)
 
-
-create_dictionary(2, 5, 8, 'cat', {'key1': 2, 'key2': 7})
