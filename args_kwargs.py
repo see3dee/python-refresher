ac29c028e979787e51775b8eba5d2d8406031c74
John Munsey <see3dee@gmail.com>
2020-09-22 13:55:58 -0700

    individual args and kwargs - more

--- args_kwargs.py
+++ args_kwargs.py
@@ -23,11 +23,36 @@
 #
 # create_dictionary(2, 5, 8, 'cat', {'key1': 2, 'key2': 7})
 #
+# def print_args(*arg):
+#     for index, value in enumerate(args):
+#         print(f' here are your indexed, unpacked arguments: {index} : {value}')
+#
 # def print_kwargs(**kwargs):
 #     for key, value in kwargs.items():
-#         print(key, value)
+#         print(f'Here are the key-value pairs of your unpacked kwargs: {key}: {value}')
 #
 #
-# print_kwargs(dog='pet', cat='coco', rat={3: 'pop'})
+# # print_kwargs(dog='pet', cat='coco', rat={3: 'pop'})
+#
+# def print_args_kwargs(*args, **kwargs):
+#     for index, value in enumerate(args):
+#         print(f' here are your indexed, unpacked arguments: {index} : {value}')
+#     for key, value in kwargs.items():
+#         print(f'Here are the key-value pairs of your unpacked kwargs: {key}: {value}')
+#
+#
+# print_args_kwargs(3, 2, 6, 'y', 'red', 'blue', {'key1': 'dog'}, dog='blue', cat=7, dict={1: 2, 3: 4, 5: 6})
+
+
+# unpack and print a list
+def print_3_things(val1, val2, val3):
+    print(f'Value1 is: {val1}, Value2 is: {val2}, Value3 is: {val3}')
+
+
+print_3_things('red', 'blue', 'yellow')
+things = ['red', 'blue', 'yellow']
+print(*things)
+
+
 
 
