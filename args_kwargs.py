15bb1d6034d24709ffacf84a934125a891987066
John Munsey <see3dee@gmail.com>
2020-09-22 10:52:11 -0700

    individual args and kwargs - more

---
+++ args_kwargs.py
@@ -0,0 +1,24 @@
+# # positional args
+# def print_individual_args(value1, value2):
+#     print(f"{value1}, {value2}")
+#
+#
+# print_individual_args({'key1': 2, 'key2': 5}, [2, 't', 5, 'h'])
+#
+#
+# # Keyword args
+# def print_individual_kwargs(value1=5, value2=7):
+#     print(f"{value1}, {value2}")
+#
+#
+# print_individual_kwargs(value1=2, value2=9)
+
+def create_dictionary(*args):
+    dict = {}
+    for i, arg in enumerate(args):
+        print(i, arg)
+        dict[i] = arg
+        print(dict)
+
+
+create_dictionary(2, 5, 8, 'cat', {'key1': 2, 'key2': 7})
