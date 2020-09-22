e8dfd1ba17d065cc00c9823321efb44f5fbc4b64
John Munsey <see3dee@gmail.com>
2020-09-22 10:04:32 -0700

    individual args and kwargs

--- scratch.py
+++
@@ -1,8 +0,0 @@
-tuple = "id", "name", "pass"
-
-print(tuple)
-
-_, name_var, pass_var = tuple
-
-print(_, name_var, pass_var)
-
---
+++ unpacking_args.py
@@ -0,0 +1,10 @@
+# def multiply(x, y=5):
+#     return x * y
+
+def multiply(*args)
+    print(args)
+
+print(multiply(3))
+
+print(multiply(3, y=10))
+
