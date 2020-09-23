103ac2e6b4b29f951963a26e3369316819ad7b8c
John Munsey <see3dee@gmail.com>
2020-09-23 13:56:59 -0700

    oop exercise - improved with list comprehension

--- oop.py
+++ oop.py
@@ -1,4 +1,4 @@
-cclass Store:
+class Store:
     def __init__(self, name):
         self.name = name
         self.items = []
@@ -6,26 +6,30 @@ cclass Store:
     def add_item(self, name, price):
         self.items.append({'name': name, 'price': price})
 
+    # def stock_price(self):
+    #     total = 0
+    #     for item in self.items:
+    #         total += item['price']
+    #     return total
+
+# list comprehension is useful here!  sweet!
     def stock_price(self):
-        total = 0
-        for item in self.items:
-            total = total + item['price']
+        total = sum([item['price'] for item in self.items])
         return total
 
-
 #  testing
-# store = Store('Safeway')
-# print(store.name)
-#
-# store.add_item('butter', 4.99)
-# store.add_item('milk', 1.99)
-# store.add_item('cheese', 2.99)
-#
-# print(store.items)
-# print(store.items[0])
-# print(len(store.items))
-#
-# print(f' stock_price is {store.stock_price()}')
+store = Store('Safeway')
+print(store.name)
+
+store.add_item('butter', 4.99)
+store.add_item('milk', 1.99)
+store.add_item('cheese', 2.99)
+
+print(store.items)
+print(store.items[0])
+print(len(store.items))
+
+print(f' stock_price is {store.stock_price()}')
 
 
 
