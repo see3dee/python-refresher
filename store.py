8ffa8748c334d4ed8d210c22d5a66f5512be19d4
John Munsey <see3dee@gmail.com>
2021-03-04 07:34:12 -0800

    store class exercise

---
+++ store.py
@@ -0,0 +1,14 @@
+class Store:
+    def __init__(self, name):
+        self.name = name
+        self.items = []
+
+    def add_item(self, name, price):
+        self.items.append({'name': name, 'price': price})
+
+    def total_price(self):
+        total = 0
+        for item in self.items:
+            total += (item['price'])
+        return total
+
